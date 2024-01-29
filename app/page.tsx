64e52c1db713bdfd2f50d9e468a15bcf0e3ebddd
James Tennant <jorahty@gmail.com>
2024-01-28 17:23:49 -0800

    spinner

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,69 @@
+'use client';
+
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
-  return <h1>Hello, World!</h1>;
+  return <Wheel />;
 }
+
+const Wheel = () => {
+  const [rotation, setRotation] = useState(0);
+  const [velocity, setVelocity] = useState(0);
+
+  let result = '';
+  const slice = Math.floor((rotation / 360) * 8);
+  if (slice == 0 || slice == 7) result = '💧';
+  else if (slice == 3 || slice == 4 || slice == 5) result = '🔥';
+  else if (slice == 6) result = '💧🔥';
+
+  const handleButtonClick = () => {
+    // Set a high initial velocity when the button is pressed
+    // setVelocity(8.43);
+    // setVelocity(11.95);
+    setVelocity(8.43 + (11.95 - 8.43) * Math.random());
+  };
+
+  useEffect(() => {
+    const updateRotation = () => {
+      // Update rotation based on velocity
+      setRotation((prevRotation) => (prevRotation + velocity) % 360);
+
+      // Gradually decrease velocity until it comes to a rest
+      setVelocity((prevVelocity) => (prevVelocity > 0 ? prevVelocity - 0.1 : 0));
+    };
+
+    // Use requestAnimationFrame to update the rotation continuously
+    const animationFrameId = requestAnimationFrame(updateRotation);
+
+    // Cleanup function to cancel animation frame on component unmount
+    return () => cancelAnimationFrame(animationFrameId);
+  }, [velocity]);
+
+  return (
+    <main style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', padding: 20 }}>
+      <div style={{ minHeight: 35, fontSize: 24 }}>{result}</div>
+      <button onClick={handleButtonClick} style={{ zIndex: 2 }}>
+        Spin
+      </button>
+      <div
+        style={{
+          borderTop: '10px solid #000',
+          borderLeft: '10px solid transparent',
+          borderRight: '10px solid transparent',
+          marginBottom: '10px',
+        }}></div>
+      <Image
+        src="/wheel.png"
+        width={2048}
+        height={2048}
+        alt="Wheel"
+        style={{
+          transform: `rotate(${rotation}deg)`,
+          height: '500px',
+          width: 'auto',
+        }}
+      />
+    </main>
+  );
+};
